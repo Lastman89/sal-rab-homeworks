1e19f8985256e239678db55bfb8e7524aff08f4d
Lastman89 <alexeygr89@gmail.com>
2022-05-18 18:53:22 +0300

    hometask_4_is_done

--- task_4/solution.js
+++ task_4/solution.js
@@ -6,7 +6,7 @@ function parseProducts(json) {
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
-    let data = {JSON.parse(`json`)};
+    let data = JSON.parse(json);
     return data.products;
 }
 
